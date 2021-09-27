39293f309106c7c1df89eb80a23c44fe2fb47ccb
Danny Kong <kongdanny123@gmail.com>
2021-09-27 17:43:01 -0400

    Initial Commit

---
+++ GreenGarden/GreenGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenGardenApp.swift
+//  GreenGarden
+//
+//  Created by Admin on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct GreenGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
